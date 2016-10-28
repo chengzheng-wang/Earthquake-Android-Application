6a65758c046a4d1696f32f6153cff48b9773564c
chengzheng-wang <cwang92@binghamton.edu>
2016-10-28 01:40:59 -0400

    create first version of layout

--- app/src/main/java/com/example/android/earthquake_application/earthquake_activity.java
+++ app/src/main/java/com/example/android/earthquake_application/earthquake_activity.java
@@ -2,6 +2,11 @@ package com.example.android.earthquake_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class earthquake_activity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class earthquake_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake_activity);
+        ArrayList<Earthquake_information> earthQuake = new ArrayList<>();
+        earthQuake.add(new Earthquake_information(7.2,"San Fransico",new Date(20160216)));
+        earthQuake.add(new Earthquake_information(7.3,"San Jose",new Date(116,03,18)));
+        earthQuake.add(new Earthquake_information(7.4,"New York",new Date(116,05,16)));
+        earthQuake.add(new Earthquake_information(7.2,"San Fransico",new Date(116,02,16)));
+        earthQuake.add(new Earthquake_information(7.3,"San Jose",new Date(116,03,18)));
+        earthQuake.add(new Earthquake_information(7.4,"New York",new Date(116,05,16)));
+        earthQuake.add(new Earthquake_information(7.2,"San Fransico",new Date(2016,02,16)));
+        earthQuake.add(new Earthquake_information(7.3,"San Jose",new Date(116,03,18)));
+        earthQuake.add(new Earthquake_information(7.4,"New York",new Date(116,05,16)));
+        earthQuake.add(new Earthquake_information(7.2,"San Fransico",new Date(2016,02,16)));
+        earthQuake.add(new Earthquake_information(7.3,"San Jose",new Date(116,03,18)));
+        earthQuake.add(new Earthquake_information(7.4,"New York",new Date(116,05,16)));
+
+
+
+        EarthquakeArrayAdapter adapter = new EarthquakeArrayAdapter(earthquake_activity.this,
+                                                                    earthQuake);
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        earthquakeListView.setAdapter(adapter);
+
     }
 }
