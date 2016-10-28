af147d9cfeb103d78a5b6a84d276cadca8002b22
chengzheng-wang <cwang92@binghamton.edu>
2016-10-28 00:12:20 -0400

    build the project

---
+++ app/src/main/java/com/example/android/earthquake_application/earthquake_activity.java
@@ -0,0 +1,13 @@
+package com.example.android.earthquake_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class earthquake_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earthquake_activity);
+    }
+}
